d46a8107aede9754302de08d7f395535930e4c1f
Adam Michalak <adam_michalak@epam.com>
2018-11-24 12:12:59 +0100

    user can define number of disks by himself

--- src/main/java/pl/michalak/adam/TowersOfHanoi.java
+++ src/main/java/pl/michalak/adam/TowersOfHanoi.java
@@ -1,20 +1,26 @@
 package pl.michalak.adam;
+
+import java.util.Scanner;
+
 /**
  * Główna klasa programu
  *
  * @author Adam Michalak
  */
 class TowersOfHanoi {
-public static void main(String[] args) {
-        char fromRod = 'A';
-        char toRod = 'B';
-        char tempRod = 'C';
-        int numberOfDisks = 3;
-        swapDisks(numberOfDisks, fromRod, toRod, tempRod);
-        System.out.printf("DONE!");
+    final static char fromRod = 'A';
+    final static char toRod = 'B';
+    final static char tempRod = 'C';
+
+    public static void main(String[] args)
+    {
+        Scanner keyboard = new Scanner(System.in);
+        swapDisks(setNumberOfDisks(keyboard), fromRod, toRod, tempRod);
+        System.out.printf("\nDONE!");
     }
 
-    private static void swapDisks(int numberOfDisks, char fromRod, char toRod, char tempRod) {
+    private static void swapDisks(int numberOfDisks, char fromRod, char toRod, char tempRod)
+    {
         if(numberOfDisks == 1) {
             System.out.printf("\nMove disk %d from rod %c to rod %c", numberOfDisks, fromRod, toRod);
             return;
@@ -22,6 +28,9 @@ public static void main(String[] args) {
         swapDisks(numberOfDisks-1, fromRod, tempRod, toRod);
         System.out.printf("\nMove disk %d from rod %c to rod %c" , numberOfDisks, fromRod, toRod);
         swapDisks(numberOfDisks-1, tempRod, toRod, fromRod);
-
+    }
+    private static int setNumberOfDisks(Scanner keyboard){
+        System.out.println("Enter number of disks: ");
+        return keyboard.nextInt();
     }
 }
