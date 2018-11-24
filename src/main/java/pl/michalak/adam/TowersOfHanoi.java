f92704d0e546ffaf08e17793b77aa88d6309b7e8
Adam Michalak <adam_michalak@epam.com>
2018-11-24 12:02:03 +0100

    add working algorithm for 3 disks

--- src/main/java/pl/michalak/adam/TowersOfHanoi.java
+++ src/main/java/pl/michalak/adam/TowersOfHanoi.java
@@ -6,6 +6,22 @@ package pl.michalak.adam;
  */
 class TowersOfHanoi {
 public static void main(String[] args) {
+        char fromRod = 'A';
+        char toRod = 'B';
+        char tempRod = 'C';
+        int numberOfDisks = 3;
+        swapDisks(numberOfDisks, fromRod, toRod, tempRod);
+        System.out.printf("DONE!");
+    }
+
+    private static void swapDisks(int numberOfDisks, char fromRod, char toRod, char tempRod) {
+        if(numberOfDisks == 1) {
+            System.out.printf("\nMove disk %d from rod %c to rod %c", numberOfDisks, fromRod, toRod);
+            return;
+        }
+        swapDisks(numberOfDisks-1, fromRod, tempRod, toRod);
+        System.out.printf("\nMove disk %d from rod %c to rod %c" , numberOfDisks, fromRod, toRod);
+        swapDisks(numberOfDisks-1, tempRod, toRod, fromRod);
 
     }
 }
