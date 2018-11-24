189403e50c30d608e34c491f16f3f7ff4a6109c1
Adam Michalak <adam_michalak@epam.com>
2018-11-24 15:20:22 +0100

    adjust game flow to use recursion

--- src/main/java/pl/michalak/adam/game_flow/GameFlowAPI.java
+++ src/main/java/pl/michalak/adam/game_flow/GameFlowAPI.java
@@ -1,5 +1,7 @@
 package pl.michalak.adam.game_flow;
 
+import pl.michalak.adam.algorithm.AlgorithmAPI;
+
 /**
  * This component is responsible for controlling the flow of this app.
  * @author Adam_Michalak
@@ -10,8 +12,10 @@ public class GameFlowAPI {
 
     public void manageFlow()
     {
-        gameFlowController.getNumbersOfDisksFromUser();
-        gameFlowController.getDecisionAboutProgressPrinting();
+        int numberOfDisks = gameFlowController.getNumbersOfDisksFromUser();
+        boolean shouldInfoBePrinted = gameFlowController.getDecisionAboutProgressPrinting();
+        AlgorithmAPI algorithmAPI = new AlgorithmAPI(shouldInfoBePrinted);
+        algorithmAPI.makeUseOfTheRecursion(numberOfDisks);
         gameFlowController.summary();
     }
 
