8c05f659b097945922ddec24932efc63629ef10e
Adam Michalak <adam_michalak@epam.com>
2018-11-24 12:25:57 +0100

    user can decide if they want to see steps of completion

--- src/main/java/pl/michalak/adam/TowersOfHanoi.java
+++ src/main/java/pl/michalak/adam/TowersOfHanoi.java
@@ -15,22 +15,33 @@ class TowersOfHanoi {
     public static void main(String[] args)
     {
         Scanner keyboard = new Scanner(System.in);
-        swapDisks(setNumberOfDisks(keyboard), fromRod, toRod, tempRod);
-        System.out.printf("\nDONE!");
+        swapDisks(setNumberOfDisks(keyboard), decideIfStepsShouldBePrinted(keyboard), fromRod, toRod, tempRod);
+        System.out.println("\nDONE!");
     }
 
-    private static void swapDisks(int numberOfDisks, char fromRod, char toRod, char tempRod)
+    private static void swapDisks(int numberOfDisks, boolean printSteps, char fromRod, char toRod, char tempRod)
     {
         if(numberOfDisks == 1) {
-            System.out.printf("\nMove disk %d from rod %c to rod %c", numberOfDisks, fromRod, toRod);
+            if(printSteps) {
+                System.out.printf("\nMove disk %d from rod %c to rod %c", numberOfDisks, fromRod, toRod);
+            }
             return;
         }
-        swapDisks(numberOfDisks-1, fromRod, tempRod, toRod);
-        System.out.printf("\nMove disk %d from rod %c to rod %c" , numberOfDisks, fromRod, toRod);
-        swapDisks(numberOfDisks-1, tempRod, toRod, fromRod);
+        swapDisks(numberOfDisks-1, printSteps, fromRod, tempRod, toRod);
+        if(printSteps) {
+            System.out.printf("\nMove disk %d from rod %c to rod %c", numberOfDisks, fromRod, toRod);
+        }
+        swapDisks(numberOfDisks-1, printSteps, tempRod, toRod, fromRod);
     }
-    private static int setNumberOfDisks(Scanner keyboard){
+    private static int setNumberOfDisks(Scanner keyboard)
+    {
         System.out.println("Enter number of disks: ");
         return keyboard.nextInt();
+    }
+    private static boolean decideIfStepsShouldBePrinted(Scanner keyboard)
+    {
+        System.out.println("Do you want to see steps needed to move the tower from one rod to the other? (Y/N) ");
+        String response = keyboard.next();
+        return (response.equals("Y") || response.equals("y"));
     }
 }
